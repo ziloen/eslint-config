e8054701fcab44cd1e40882bcdb7739073a72316
neo <neo@neo.neo>
2022-04-25 14:43:22 +0800

    chore

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -16,10 +16,16 @@ module.exports = {
     'no-promise-executor-return': 'error',
     /** 这可能会导致数据竞争 */
     'require-atomic-updates': 'error',
-    // 限制最大回调嵌套数量
+    /** 限制最大回调嵌套数量 */
     'max-nested-callbacks': ['warn', 5],
     /** 禁止不必要的 await */
     '@typescript-eslint/await-thenable': 'warn',
+    /** for 与 await 一起使用会阻塞线程，可以使用 Promise.all() 一次发起多个 Promise */
+    'no-await-in-loop': 'warn',
+    /** 返回 promise 的函数必须有 async 关键字 */
+    '@typescript-eslint/promise-function-async': ['warn', {
+      checkArrowFunctions: false,
+    }],
     /** 不要嵌套 promise */
     'no-async-promise-executor': 'warn',
     /** 同步发方法会阻塞线程，使用异步方法代替 */
