c939ed910c7f1a276330bdfa04cb2f8d879937bb
Neo <neo@neo.neo>
2022-05-13 23:25:52 +0800

    chore

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -9,11 +9,16 @@ module.exports = {
     'no-constant-binary-expression': 'error',
     'no-constructor-return': 'warn',
     'no-eval': 'warn',
+    'no-unused-vars': 'off',
     'no-useless-rename': 'warn',
     'no-var': 'error',
     'prefer-const': ['warn', {
       destructuring: 'all',
       ignoreReadBeforeAssign: true
     }],
+
+
+    // unicorn https://github.com/sindresorhus/eslint-plugin-unicorn
+    'unicorn/better-regex': 'warn',
   }
 }
