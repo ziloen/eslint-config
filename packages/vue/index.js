acec0296b931f154ed4f2f6226c9df5a4bd01361
Neo <neo@neo.neo>
2022-05-16 01:57:27 +0800

    chore

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -14,12 +14,13 @@ module.exports = {
     '@ziloen/eslint-config-typescript'
   ],
   rules: {
-    'vue/max-attributes-per-line': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off',
     'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
 
     // reactivity transform
     'vue/no-setup-props-destructure': 'off',
