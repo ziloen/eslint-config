12817c733fda85b110111621c801123917843955
N <34268416+ziloen@users.noreply.github.com>
2022-08-23 01:34:20 +0800

    chore: add rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -20,11 +20,46 @@ module.exports = {
     'prefer-exponentiation-operator': 'warn',
     'prefer-object-has-own': 'warn',
 
+    // -------------------------------------------------------------
     // unicorn https://github.com/sindresorhus/eslint-plugin-unicorn
+    // -------------------------------------------------------------
     'unicorn/better-regex': ['warn', {
       sortCharacterClasses: false
     }],
     'unicorn/no-instanceof-array': 'warn',
     'unicorn/no-invalid-remove-event-listener': 'error',
+    'unicorn/consistent-function-scoping': ['warn', {
+      checkArrowFunctions: false
+    }],
+    'unicorn/error-message': 'warn',
+    'unicorn/expiring-todo-comments': 'warn',
+    'unicorn/no-document-cookie': 'warn',
+
+    'no-nested-ternary': 'off',
+    'unicorn/no-nested-ternary': 'warn',
+
+    'unicorn/no-new-buffer': 'error',
+    'unicorn/no-thenable': 'error',
+    'unicorn/no-unreadable-array-destructuring': 'error',
+    'unicorn/no-useless-fallback-in-spread': 'warn',
+    'unicorn/no-useless-promise-resolve-reject': 'warn',
+    'unicorn/no-useless-spread': 'warn',
+    'unicorn/no-useless-undefined': ['warn', {
+      checkArguments: false
+    }],
+    'unicorn/no-zero-fractions': 'warn',
+    'unicorn/number-literal-case': 'warn',
+    'unicorn/prefer-at': 'warn',
+    'unicorn/prefer-code-point': 'warn',
+    'unicorn/prefer-dom-node-text-content': 'warn',
+    'unicorn/prefer-dom-node-append': 'warn',
+    'unicorn/prefer-export-from': ['warn', {
+      ignoreUsedVariables: true
+    }],
+    'unicorn/prefer-keyboard-event-key': 'warn',
+    'unicorn/prefer-modern-dom-apis': 'warn',
+    'unicorn/prefer-node-protocol': 'warn',
+    'unicorn/prefer-query-selector': 'warn',
+    'unicorn/prefer-string-slice': 'warn',
   }
 }
