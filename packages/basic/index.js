289a20e7e55d4e4f5c2ba2dde3145fc9a537e7b1
N <34268416+ziloen@users.noreply.github.com>
2022-10-01 01:59:57 +0800

    chore: new rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     'unicorn',
   ],
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
   ignorePatterns: [
     '*.min.*',
     '*.d.ts',
@@ -14,21 +14,36 @@ module.exports = {
     '/node_modules/**/*.*'
   ],
   rules: {
+    /** 检查数组方法返回值 */
+    'array-callback-return': ['error', { 'allowImplicit': true }],
+    /** 倾向使用全等 === */
     'eqeqeq': ['warn', 'smart'],
+    /** `a = a || b` 简写为 `a ||= b` */
+    'logical-assignment-operators': ['warn'],
+    /** 限制最大回调嵌套数量 */
+    'max-nested-callbacks': ['warn', 5],
+    /** for 与 await 一起使用会串行阻塞线程，可以使用 Promise.all() 一次发起多个 Promise */
+    'no-await-in-loop': 'warn',
+    /** 不要嵌套 promise */
+    'no-async-promise-executor': 'warn',
+    /** 别用 */
     'no-caller': 'error',
+    /** 错误使用 */
     'no-constant-binary-expression': 'error',
+    /** console.log使用后删除 */
+    'no-console': ['warn', {
+      allow: ['warn', 'error']
+    }],
+    /** `constructor` 中不应有返回值(允许作为控制流使用) */
     'no-constructor-return': 'warn',
     'no-empty': 'off',
     'no-eval': 'warn',
-    'no-unused-vars': 'off',
-    'no-useless-rename': 'warn',
-    'no-var': 'error',
-    'prefer-const': ['warn', {
-      destructuring: 'all',
-      ignoreReadBeforeAssign: true
+    /** 防止`switch case`忘写`break` */
+    'no-fallthrough': ['error', {
+      allowEmptyCase: true
     }],
-    'prefer-exponentiation-operator': 'warn',
-    'prefer-object-has-own': 'warn',
+    /** Promise 内 return 没有意义，使用 resolve 或 reject */
+    'no-promise-executor-return': 'error',
     /** 令人混淆的 window 上的变量 */
     'no-restricted-globals': [
       'error',
@@ -37,6 +52,29 @@ module.exports = {
       'length',
       'status'
     ],
+    /** 禁止不必要的 await */
+    'no-return-await': 'warn',
+    'no-unused-vars': 'off',
+    'no-useless-rename': 'warn',
+    'no-var': 'error',
+    'object-shorthand': ['warn', 'always', {
+      avoidQuotes: true
+    }],
+
+    'prefer-const': ['warn', {
+      destructuring: 'all',
+      ignoreReadBeforeAssign: true
+    }],
+    /** 使用 `a ** b` 替代 `Math.pow(a, b)` */
+    'prefer-exponentiation-operator': 'warn',
+    /** 使用 `Object.hasOwn()` 替代 `Object.prototype.hasOwnProperty.call()` */
+    'prefer-object-has-own': 'warn',
+    /** 这可能会导致数据竞争 */
+    'require-atomic-updates': 'error',
+
+    // Node 插件 未安装
+    /** 同步发方法会阻塞线程，使用异步方法代替 */
+    // 'node/no-sync': 'warn',
 
     // -------------------------------------------------------------
     // unicorn https://github.com/sindresorhus/eslint-plugin-unicorn
@@ -46,17 +84,16 @@ module.exports = {
     }],
     'unicorn/no-instanceof-array': 'warn',
     'unicorn/no-invalid-remove-event-listener': 'error',
-    // 'unicorn/consistent-function-scoping': ['warn', {
-    //   checkArrowFunctions: false
-    // }],
     'unicorn/error-message': 'warn',
     'unicorn/expiring-todo-comments': 'warn',
     'unicorn/no-document-cookie': 'warn',
 
+    /** 警告嵌套三元运算符 */
     'no-nested-ternary': 'off',
     'unicorn/no-nested-ternary': 'warn',
 
     'unicorn/no-new-buffer': 'error',
+    /** 禁止声明`then`属性，以免和`Promsie`混淆 */
     'unicorn/no-thenable': 'error',
     'unicorn/no-unreadable-array-destructuring': 'error',
     'unicorn/no-useless-fallback-in-spread': 'warn',
--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -12,46 +12,30 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   rules: {
+    /** 禁止不必要的 await */
+    '@typescript-eslint/await-thenable': 'warn',
+    /** 允许空函数 */
+    '@typescript-eslint/no-empty-function': 'off',
+    /** 允许显式 any */
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    /** 允许非空断言 */
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    /** 允许 TypeScript 重载声明 */
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': ['error'],
+    /** 允许未使用变量 */
+    '@typescript-eslint/no-unused-vars': 'off',
+    /** 使用可选链`a?.b`替代`a && a.b` */
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    /** 返回 promise 的函数必须有 async 关键字 */
+    '@typescript-eslint/promise-function-async': ['warn', {
+      checkArrowFunctions: false,
+    }],
     /** 模板字符串只允许数字字符串 */
     '@typescript-eslint/restrict-template-expressions': ['error', {
       allowNumber: true,
       // allowBoolean: true,
     }],
-    /** 允许空函数 */
-    '@typescript-eslint/no-empty-function': 'off',
-    /** console.log使用后删除 */
-    'no-console': ['warn', {
-      allow: ['warn', 'error']
-    }],
-    /** 禁止不必要的 await */
-    'no-return-await': 'warn',
-    // Promise 内 return 没有意义，使用 resolve 或 reject
-    'no-promise-executor-return': 'error',
-    /** 这可能会导致数据竞争 */
-    'require-atomic-updates': 'error',
-    /** 限制最大回调嵌套数量 */
-    'max-nested-callbacks': ['warn', 5],
-    /** 禁止不必要的 await */
-    '@typescript-eslint/await-thenable': 'warn',
-    /** for 与 await 一起使用会阻塞线程，可以使用 Promise.all() 一次发起多个 Promise */
-    'no-await-in-loop': 'warn',
-    /** 返回 promise 的函数必须有 async 关键字 */
-    '@typescript-eslint/promise-function-async': ['warn', {
-      checkArrowFunctions: false,
-    }],
-    /** 不要嵌套 promise */
-    'no-async-promise-executor': 'warn',
-    /** 同步发方法会阻塞线程，使用异步方法代替 */
-    // 'node/no-sync': 'warn',
-    /** 允许显式 any */
-    '@typescript-eslint/no-explicit-any': 'off',
-    /** 允许不适用变量 */
-    '@typescript-eslint/no-unused-vars': 'off',
-
-    // 允许重载声明
-    "no-redeclare": "off",
-    "@typescript-eslint/no-redeclare": ["error"],
-    /** 允许非空断言 */
-    '@typescript-eslint/no-non-null-assertion': 'off'
   }
 }
