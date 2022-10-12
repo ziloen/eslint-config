c36f7c897641f3b1ac1aa3c0499e3e29d17f7b4e
N <zhupengcheng5@163.com>
2022-10-12 18:01:50 +0800

    chore

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -7,13 +7,16 @@ module.exports = {
     }
   ],
   extends: [
+    '@ziloen/eslint-config-basic',
     'plugin:@typescript-eslint/recommended',
-    '@ziloen/eslint-config-basic'
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict'
   ],
   parser: '@typescript-eslint/parser',
   rules: {
     /** 禁用默认`no-undef`，eslint 不会检查`*.d.ts`，导致误报全局变量与类型不存在 */
     'no-undef': 'off',
+
     /** 禁止不必要的 await */
     '@typescript-eslint/await-thenable': 'warn',
     /** 允许空函数 */
@@ -23,11 +26,17 @@ module.exports = {
 
     /** 允许非空断言 */
     '@typescript-eslint/no-non-null-assertion': 'off',
+
     /** 允许 TypeScript 重载声明 */
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': ['error'],
     /** 允许未使用变量 */
     '@typescript-eslint/no-unused-vars': 'off',
+
+    /** 超出范围的数字 */
+    'no-loss-of-precision': 'off',
+    '@typescript-eslint/no-loss-of-precision': 'error',
+
     /** 使用可选链`a?.b`替代`a && a.b` */
     '@typescript-eslint/prefer-optional-chain': 'warn',
     /** 返回 promise 的函数必须有 async 关键字 */
