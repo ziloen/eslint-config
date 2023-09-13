a60021ac917c22135ca979f736917eba4cef3612
N <34268416+ziloen@users.noreply.github.com>
2023-09-13 22:05:20 +0800

    chore: update generic-spacing

--- packages/plugin/src/rules/generic-spacing.test.ts
+++ packages/plugin/src/rules/generic-spacing.test.ts
@@ -6,11 +6,24 @@ const valid = [
   `type Foo<
   T,
   K,
-> = T`
+> = T`,
+  `type Foo<T, K> = Record<T, K>`,
+  `type Foo<T, K> = Record<
+  T,
+  K
+>`
 ]
 
 const invalid = [
-  [`type Foo< T, K > = T`, `type Foo<T, K> = T`, 2]
+  [`type Foo< T, K > = T`, `type Foo<T, K> = T`, 2],
+  [`type Foo<T, K> = Record< T, K >`, `type Foo<T, K> = Record<T, K>`, 2],
+  [`type Foo<   T, K > = Record< T, K >`, `type Foo<T, K> = Record<T, K>`, 4],
+  [`type Foo<
+T, K
+> = Record< T, K >`, `type Foo<
+T, K
+> = Record<T, K>`, 2],
+  [`type Foo<T=any, K=any> = T`, `type Foo<T = any, K = any> = T`, 2]
 ] as const
 
 const ruleTester: RuleTester = new RuleTester({
--- packages/plugin/src/rules/generic-spacing.ts
+++ packages/plugin/src/rules/generic-spacing.ts
@@ -1,6 +1,6 @@
 import { TSESTree } from '@typescript-eslint/utils'
 import { createEslintRule } from '../utils'
-
+import { RuleContext } from '@typescript-eslint/utils/ts-eslint'
 
 // TSTypeParameterDeclaration type A"<T, S>"
 // TSTypeParameterInstantiation Record"<T, any>"
@@ -10,6 +10,55 @@ export const RULE_NAME = 'generic-spacing'
 export type MessageIds = 'genericSpacingMismatch'
 export type Options = []
 
+const messageId = 'genericSpacingMismatch'
+
+function spacingAround(
+  node: TSESTree.TSTypeParameterInstantiation | TSESTree.TSTypeParameterDeclaration,
+  context: Readonly<RuleContext<MessageIds, []>>
+) {
+  const params = node.params
+  if (!params.length) return
+
+  const sourceCode = context.sourceCode
+
+  const start = node.range[0] + 1
+  const end = node.range[1] - 1
+
+  const startNode = params[0]
+  const endNode = params.at(-1)!
+
+  const textBeforeFirst = sourceCode.text.slice(start, startNode.range[0])
+  const textAfterLast = sourceCode.text.slice(endNode.range[1], end)
+
+  // if same line, remove spaces
+  if (textBeforeFirst.length && !textBeforeFirst.includes('\n')) {
+    context.report({
+      node,
+      messageId,
+      *fix(fixer) {
+        yield fixer.replaceTextRange([start, startNode.range[0]], '')
+      }
+    })
+  }
+
+  // if same line, remove spaces
+  if (textAfterLast.length && !textAfterLast.includes('\n')) {
+    context.report({
+      node,
+      messageId,
+      *fix(fixer) {
+        yield fixer.replaceTextRange([endNode.range[1], end], '')
+      },
+      loc: {
+        start: endNode.loc.end,
+        end: {
+          column: node.loc.end.column - 1,
+          line: node.loc.end.line
+        }
+      }
+    })
+  }
+}
 
 export default createEslintRule<Options, MessageIds>({
   name: RULE_NAME,
@@ -30,46 +79,29 @@ export default createEslintRule<Options, MessageIds>({
     const sourceCode = context.sourceCode
 
     return {
-      TSTypeParameterDeclaration(node) {
-        const params = node.params
-        if (!params.length) return
-
-        const start = node.range[0] + 1
-        const end = node.range[1] - 1
-
-        const firstParam = params[0]
-        const lastParam = params.at(-1)!
-
-        const textBeforeFirst = sourceCode.text.slice(start, firstParam.range[0])
-        const textAfterLast = sourceCode.text.slice(lastParam.range[1], end)
-
-        // if same line, remove spaces
-        if (textBeforeFirst.length && !textBeforeFirst.includes('\n')) {
-          context.report({
-            node,
-            messageId: 'genericSpacingMismatch',
-            *fix(fixer) {
-              yield fixer.replaceTextRange([start, firstParam.range[0]], '')
-            }
-          })
-        }
-
-        // if same line, remove spaces
-        if (textAfterLast.length && !textAfterLast.includes('\n')) {
-          context.report({
-            node,
-            messageId: 'genericSpacingMismatch',
-            *fix(fixer) {
-              yield fixer.replaceTextRange([lastParam.range[1], end], '')
-            }
-          })
-        }
-      },
-      TSTypeParameterInstantiation(node) {
-
-      },
+      TSTypeParameterDeclaration: node => spacingAround(node, context),
+      TSTypeParameterInstantiation: node => spacingAround(node, context),
       TSTypeParameter(node) {
+        if (!node.default) return
+        const startNode = node.constraint || node.name
+        const endNode = node.default
 
+        const start = startNode.range[1]
+        const end = endNode.range[0]
+
+        if (sourceCode.text.slice(start, end) !== ' = ') {
+          context.report({
+            node,
+            messageId,
+            *fix(fixer) {
+              yield fixer.replaceTextRange([start, end], ' = ')
+            },
+            loc: {
+              start: startNode.loc.end,
+              end: endNode.loc.start
+            }
+          })
+        }
       }
     }
   }
