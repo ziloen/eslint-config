8d3b33acb8365c10f8e6d22423234bfb0abb285e
N <34268416+ziloen@users.noreply.github.com>
2025-09-26 11:58:53 +0800

    update deps

--- src/configs/index.ts
+++
@@ -1,6 +0,0 @@
-export { format } from './format'
-export { javascript } from './javascript'
-export { react } from './react'
-export { typescript } from './typescript'
-export { vue } from './vue'
-
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export * from './configs'
+export { format } from './configs/format'
+export { javascript } from './configs/javascript'
+export { react } from './configs/react'
+export { typescript } from './configs/typescript'
+export { vue } from './configs/vue'
 export type { ConfigArray } from './types'
 
